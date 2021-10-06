caaaf0e839a4d2df10bb48df1bb54b2f22c9c096
HanUseArch <wooyeophan@gmail.com>
2021-10-06 11:48:48 +0900

    Coded Again Again. For prefection. ~~Apple.~~

--- Queen/QueenNxNRestart.cpp
+++ Queen/QueenNxNRestart.cpp
@@ -1,67 +1,81 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 
-#define BLANK '0'
-#define QUEEN 'Q'
+char BLANK;
+char QUEEN;
 
 std::vector<std::vector<char>> solution(int n);
 
-int main()
+int main(int argc, char** argv)
 {
-  for(auto&& i : solution(8))
+  if(argc > 1 && argc < 4)
+  {
+    QUEEN = argv[1][0];
+    BLANK = argv[2][0];
+  }
+  else
+  {
+    std::cout << "USAGE: program QUEEN BLANK" << std::endl;
+    return(0);
+  }
+
+  int n;
+  std::cout << "Input: ";
+  std::cin >> n;
+
+  for(auto&& i : solution(n))
   {
     for(char j : i)
     {
-      printf("%c", j);
+      std::cout << j;
     }
-    printf("\n");
+    std::cout << "\n";
   }
+
+  return(0);
 }
 
 
 std::vector<std::vector<char>> solution(int n)
 {
-    std::vector<std::vector<char>> answer(std::vector(n, std::vector(n, BLANK)));
+  std::vector<std::vector<char>> answer(std::vector(n, std::vector(n, BLANK)));
+  
+  bool isOdd = n & 1;
+  n = isOdd ? n - 1 : n;
 
-    bool isOdd = n & 1;
-    n = isOdd ? n - 1 : n;
+  int halfN = n / 2;
+  int quaterN = n / 4;
 
-    int quaterN = n / 4;
-    int halfN = n / 2;
+  answer[quaterN][0] = QUEEN;
+  answer[0][halfN] = QUEEN;
 
-    // y = x/2;
-    
-    answer[0][halfN] = QUEEN;
-    answer[quaterN][0] = QUEEN;
-
-
-    if(n >= 8)
+  if(n >= 8)
+  {
+    for(int i = 0; i < halfN; ++++i) // y = x/2
     {
-        for(int i = 0; i < halfN - 1; ++++i)
-        {
-            answer[quaterN - i / 2][i] = QUEEN;
-            answer[halfN - i / 2][halfN + i] = QUEEN;
-        }
+      answer[quaterN - i / 2][i] = QUEEN;
+      answer[halfN - i / 2][halfN + i] = QUEEN;
+      // [y][x]
     }
+  }
 
-    for(int i = 0; i < halfN; ++i)
+  for(int i = 0; i < halfN; ++i)
+  {
+    for(int j = 0; j < n; ++j)
     {
-        for(int j = 0; j < n; ++j)
-        {
-            answer[n - 1 - i][n - 1 - j] = answer[i][j];
-        }
+      answer[n - i - 1][n - j - 1] = answer[i][j];
     }
+  }
 
-    if(isOdd)
+  if(isOdd)
+  {
+    for(int i = 0; i < n; ++i)
     {
-      for(int i = 0; i < n; ++i)
-      {
-        answer[i][n] = BLANK;
-        answer[n][i] = BLANK;
-      }
-      answer[n][n] = QUEEN;
+      answer[i][n] = BLANK;
+      answer[n][i] = BLANK;
     }
+    answer[n][n] = QUEEN;
+  }
 
-    return answer;
-
+  return answer;
 }
